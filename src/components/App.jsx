4e94f5dc9423f9edc7aaaef62350d61a272ef434
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-04-04 22:33:34 +0300

    fix header logo not loading in build version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Location from './Location.jsx';
 import travelData from '../data/travel.js';
+import headerLogo from '../assets/header-logo.svg';
 
 export default function App() {
   const data = travelData.map((item, id) => {
@@ -11,7 +12,7 @@ export default function App() {
   return (
     <>
       <header className="primary-header">
-        <img src="assets/header-logo.svg" className='header-logo' width="24px" height="24px" alt="" />
+        <img src={headerLogo} className='header-logo' width="24px" height="24px" alt="" />
         <h1 className="header-title">my travel journal.</h1>
       </header>
       <main>
