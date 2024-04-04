ab8c076f572ddbbc9608f901c88de96c174f32f5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-04-04 18:09:04 +0300

    adjust layout of each article for mobile

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -5,20 +5,22 @@ export default function Location(props) {
   const {location, country, googleMapsUrl, startDate, endDate, description, imageUrl} = props.item;
 
   return (
-    <article>
+    <article className='location-root-container'>
       <div>
         <header>
-          <div>
-            <img src={locationMarker} width="7px" height="10px" alt="" />
-            <h2 className='country'>{country}</h2>
-            <a href={googleMapsUrl} className='google-maps-url'>View on Google Maps</a>
+          <div className='location-url-container'>
+            <img src={locationMarker} className='location-marker' width="7px" height="10px" alt="" />
+            <h2 className='location-country'>{country}</h2>
+            <a href={googleMapsUrl} className='location-google-url'>View on Google Maps</a>
           </div>
-          <h3 className='location'>{location}</h3>
+          <h3 className='location-name'>{location}</h3>
         </header>
-        <p className='duration'>{startDate} - {endDate}</p>
-        <p>{description}</p>
+        <p className='location-duration'>{startDate} - {endDate}</p>
+        <p className='location-description'>{description}</p>
       </div>
-      <img src={imageUrl} alt={location} />
+      <div className='location-image-container'>
+        <img src={imageUrl} alt={location} className='location-image' />
+      </div>
     </article>
   )
 }
