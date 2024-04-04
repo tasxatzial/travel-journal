d9e4a09e3976050621b09308812c303051f20827
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-04-04 21:50:12 +0300

    inform screen readers about the name of each article

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import travelData from '../data/travel.js';
 export default function App() {
   const data = travelData.map((item, id) => {
     return (
-      <Location key={id} item={item}/>
+      <Location key={id} id={id} item={item}/>
     )
   });
   return (
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import locationMarker from '../assets/location-marker.svg';
 
 export default function Location(props) {
-  const {id, location, country, googleMapsUrl, startDate, endDate, description, imageUrl} = props.item;
+  const {location, country, googleMapsUrl, startDate, endDate, description, imageUrl} = props.item;
+  const id = location.split(' ').join('-') + '-' + props.id;
 
   return (
-    <article className='location-root-container'>
+    <article className='location-root-container' aria-labelledby={id}>
       <div className='location-details-container'>
         <header>
           <div className='location-url-container'>
@@ -13,7 +14,7 @@ export default function Location(props) {
             <h3 className='location-country'>{country}</h3>
             <a href={googleMapsUrl} className='location-google-url'>View on Google Maps</a>
           </div>
-          <h2 className='location-name'>{location}</h2>
+          <h2 id={id} className='location-name'>{location}</h2>
         </header>
         <p className='location-duration'>{startDate} - {endDate}</p>
         <p className='location-description'>{description}</p>
