04a7f105e4a1937a8a41b1bcf4f0e9c32f9d1f67
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-04-30 04:15:00 +0300

    minor refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Location from './Location.jsx';
-import travelData from '../data/travel.js';
-import headerLogo from '../assets/header-logo.svg';
+import Location from './components/Location.jsx';
+import travelData from './data/travel.js';
+import headerLogo from './assets/header-logo.svg';
 
 export default function App() {
   const data = travelData.map(item => {
