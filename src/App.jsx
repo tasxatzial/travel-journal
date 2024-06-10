c5dedc7f8ad596337d3a92c27f0abf3ad7a5b2b2
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-06-10 19:21:59 +0300

    use double quotes in html markup

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <div className="page-container">
       <header className="primary-header">
-        <img src={headerLogo} className='header-logo' width="24px" height="24px" alt="" />
+        <img src={headerLogo} className="header-logo" width="24px" height="24px" alt="" />
         <h1 className="header-title">my travel journal.</h1>
       </header>
       <main>
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -6,21 +6,21 @@ export default function Location(props) {
   const id = React.useId();
 
   return (
-    <article className='location-root-container' aria-labelledby={id}>
-      <div className='location-details-container'>
+    <article className="location-root-container" aria-labelledby={id}>
+      <div className="location-details-container">
         <header>
-          <div className='location-url-container'>
-            <img src={locationMarker} className='location-marker' width="7px" height="10px" alt="" />
-            <h3 className='location-country'>{country}</h3>
-            <a href={googleMapsUrl} className='location-google-url'>View on Google Maps</a>
+          <div className="location-url-container">
+            <img src={locationMarker} className="location-marker" width="7px" height="10px" alt="" />
+            <h3 className="location-country">{country}</h3>
+            <a href={googleMapsUrl} className="location-google-url">View on Google Maps</a>
           </div>
-          <h2 id={id} className='location-name'>{location}</h2>
+          <h2 id={id} className="location-name">{location}</h2>
         </header>
-        <p className='location-duration'>{startDate} - {endDate}</p>
-        <p className='location-description'>{description}</p>
+        <p className="location-duration">{startDate} - {endDate}</p>
+        <p className="location-description">{description}</p>
       </div>
-      <div className='location-image-container'>
-        <img src={imageUrl} alt={location} className='location-image' />
+      <div className="location-image-container">
+        <img src={imageUrl} alt={location} className="location-image" />
       </div>
     </article>
   )
