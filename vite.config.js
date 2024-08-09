ffb7897f0963bf18a30ab6d1900a4150b4aa67f6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-08-10 00:40:53 +0300

    add github workflow

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: "src",
+  base: "/travel-journal/",
   build: {
     emptyOutDir: true,
     outDir: '../dist',
